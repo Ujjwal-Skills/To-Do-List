2d12ac3e82a1e09ebbbba05b1210be1b69cfacc4
Ujjwal-Skills <ujjwalskills2025@gmail.com>
2026-03-29 12:28:30 +0530

    currently program can show menu, add task but not save it, and view tasks

---
+++ index.py
@@ -0,0 +1,33 @@
+#To do List
+
+# show menu
+print("""1. Add Task
+2. View Tasks
+3. Exit""")
+
+#user make choice from menu by entering the number
+choice = int(input("Enter your choice by entering the number: "))
+
+tasks = [] #add tasks entered by user
+
+#if user choose option 1
+if choice == 1:
+    task_name = input("Enter the task: ")
+    tasks.append(task_name)
+    print("Task added successfully!")
+    #print(tasks)
+
+#if user choose option 2
+if choice == 2:
+    if len(tasks) == 0:
+        print("No tasks yet!")
+    else:
+        for i in range(len(tasks)):
+            #print(i+1,".",tasks[i])
+            print(f"{i+1}. {tasks[i]}")
+
+        '''i = 1
+        for task in tasks:
+            print(i,".",task)
+            i += 1'''
+        
