e2a2a6cd5a77f1ece3b0790c3773b09dd1496a62
Ujjwal-Skills <ujjwalskills2025@gmail.com>
2026-03-29 12:39:38 +0530

    making program to run until user select exit

--- index.py
+++ index.py
@@ -1,33 +1,41 @@
 #To do List
 
-# show menu
-print("""1. Add Task
-2. View Tasks
-3. Exit""")
+#making program to run until user select exit
+while True:
 
-#user make choice from menu by entering the number
-choice = int(input("Enter your choice by entering the number: "))
+    # show menu
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
 
-tasks = [] #add tasks entered by user
+    #user make choice from menu by entering the number
+    choice = int(input("Enter your choice by entering the number: "))
 
-#if user choose option 1
-if choice == 1:
-    task_name = input("Enter the task: ")
-    tasks.append(task_name)
-    print("Task added successfully!")
-    #print(tasks)
+    tasks = [] #add tasks entered by user
 
-#if user choose option 2
-if choice == 2:
-    if len(tasks) == 0:
-        print("No tasks yet!")
-    else:
-        for i in range(len(tasks)):
-            #print(i+1,".",tasks[i])
-            print(f"{i+1}. {tasks[i]}")
+    #if user choose option 1
+    if choice == 1:
+        task_name = input("Enter the task: ")
+        tasks.append(task_name)
+        print("Task added successfully!")
+        #print(tasks)
 
-        '''i = 1
-        for task in tasks:
-            print(i,".",task)
-            i += 1'''
+    #if user choose option 2
+    elif choice == 2:
+        if len(tasks) == 0:
+            print("No tasks yet!")
+        else:
+            for i in range(len(tasks)):
+                #print(i+1,".",tasks[i])
+                print(f"{i+1}. {tasks[i]}")
+
+            '''i = 1
+            for task in tasks:
+                print(i,".",task)
+                i += 1'''
+            
+    #if user choose option 3    
+    elif choice == 3:
+        print("Goodbye!")
+        break
         
